5f9b93f24345f632aa5918a51ad0616320c9cf8b
unknown <tigerpython101@gmail.com>
2024-10-13 22:59:03 +0700

    Add credit card content layout

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,19 @@
 import "./Cards.css";
-import background from "../assets/images/bg-main-desktop.png"
-import front from "../assets/images/bg-card-front.png"
-import back from "../assets/images/bg-card-back.png"
+import background from "../assets/images/bg-main-desktop.png";
+import logo from "../assets/images/card-logo.svg";
 
 export default function Cards() {
     return (
         <div className="cards">
             <img src={background} alt="background" />
             <div className="card" id="front">
-                <img src={front} alt="Front of card" />
+                <img src={logo} alt="Credit card logo" id="logo" />
+                <p id="card-number">0000 0000 0000 0000</p>
+                <p id="full-name" class="small" >JANE APPLESEED</p>
+                <p id="exp-date" class="small" >00/00</p>
             </div>
             <div className="card" id="back">
-                <img src={back} alt="Back of card" />
+                <p id="cvc" class="small" >000</p>
             </div>
         </div>
     )
