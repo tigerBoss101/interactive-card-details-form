4abb0caaf62d1cf5c9e529142190673f1850fd36
unknown <tigerpython101@gmail.com>
2024-10-14 18:55:56 +0700

    change class to className

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,11 +9,11 @@ export default function Cards() {
             <div className="card" id="front">
                 <img src={logo} alt="Credit card logo" id="logo" />
                 <p id="card-number">0000 0000 0000 0000</p>
-                <p id="full-name" class="small" >JANE APPLESEED</p>
-                <p id="exp-date" class="small" >00/00</p>
+                <p id="full-name" className="small" >JANE APPLESEED</p>
+                <p id="exp-date" className="small" >00/00</p>
             </div>
             <div className="card" id="back">
-                <p id="cvc" class="small" >000</p>
+                <p id="cvc" className="small" >000</p>
             </div>
         </div>
     )
